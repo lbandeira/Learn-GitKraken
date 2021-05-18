b0d0d03d7a18ccfffa16a662815efba9720fd342
Laís Bandeira <lbms@cin.ufpe.br>
2021-05-18 02:11:41 -0300

    Add another function

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ def stringConverter(string_to_convert):
     res = string_to_convert.upper()
     return res
 
+def stringConverter2(string_to_convert1, string_to_convert2):
+    res = string_to_convert1.upper() + string_to_convert2.upper()
+    return res
+
 str_input = input("Please enter a phrase\nlais")
 output = stringConverter(str_input)
 
