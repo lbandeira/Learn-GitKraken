fe59181ad3c65bb9a0ffa3836a5939d5e8583212
Laís Bandeira <lbms@cin.ufpe.br>
2021-05-18 02:01:29 -0300

    Add a branch

---
+++ main.py
@@ -0,0 +1,9 @@
+
+def stringConverter(string_to_convert):
+    res = string_to_convert.upper()
+    return res
+
+str_input = input("Please enter a phrase\nlais")
+output = stringConverter(str_input)
+
+print(output)
